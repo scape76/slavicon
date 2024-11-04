20828ba41362708a8f1c7ce8e9bdaf337f4a61d6
Yaroslav <yaroslavkoshylko@gmail.com>
2024-11-04 19:52:29 +0200

    visual changes collection/main pages

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,22 +6,22 @@ import { RootNotFound } from "./not-found";
 
 // Set up a Router instance
 const router = createRouter({
-  routeTree,
-  defaultPreload: "intent",
-  defaultNotFoundComponent: () => <RootNotFound />,
-  notFoundMode: "fuzzy",
+   routeTree,
+   defaultPreload: "intent",
+   defaultNotFoundComponent: () => <RootNotFound />,
+   notFoundMode: "fuzzy",
 });
 
 // Register things for tyspesafety
 declare module "@tanstack/react-router" {
-  interface Register {
-    router: typeof router;
-  }
+   interface Register {
+      router: typeof router;
+   }
 }
 
 const rootElement = document.getElementById("app")!;
 
 if (!rootElement.innerHTML) {
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(<RouterProvider router={router} />);
+   const root = ReactDOM.createRoot(rootElement);
+   root.render(<RouterProvider router={router} />);
 }
