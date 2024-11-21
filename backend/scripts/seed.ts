25703770989490cefb3bfef614728843f62e4196
Yaroslav <yaroslavkoshylko@gmail.com>
2024-11-21 16:43:18 +0200

    bg changes

--- backend/scripts/seed.ts
+++ backend/scripts/seed.ts
@@ -3,18 +3,17 @@ import { gods } from "../src/db/schema";
 import cozyGods from "./gods.json";
 
 export const seed = async () => {
-  await db.delete(gods);
-  const result = await db.insert(gods).values(cozyGods);
-  return result.rowCount;
+   const result = await db.insert(gods).values(cozyGods);
+   return result.rowCount;
 };
 
 seed()
-  .then((count) => {
-    console.log(`Seed completed! ${count} gods inserted.`);
-    process.exit(0);
-  })
-  .catch((err) => {
-    console.error("Seed failed!");
-    console.error(err);
-    process.exit(1);
-  });
+   .then((count) => {
+      console.log(`Seed completed! ${count} gods inserted.`);
+      process.exit(0);
+   })
+   .catch((err) => {
+      console.error("Seed failed!");
+      console.error(err);
+      process.exit(1);
+   });
--- frontend/src/routes/collection/route.tsx
+++ frontend/src/routes/collection/route.tsx
@@ -4,21 +4,22 @@ import { createFileRoute, useRouter } from "@tanstack/react-router";
 import { Outlet } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/collection")({
-  component: () => {
-    const router = useRouter();
+   component: () => {
+      const router = useRouter();
 
-    return (
-      <div
-        className={cn(
-          "flex flex-col h-dvh bg-[url('/background/background-chat.png')] bg-fixed bg-no-repeat bg-cover bg-[-40vw_center] md:bg-[center_center]  xl:bg-cover",
-          {
-            "overflow-hidden": router.state.location.pathname === "/collection",
-          },
-        )}
-      >
-        <Header />
-        <Outlet />
-      </div>
-    );
-  },
+      return (
+         <div
+            className={cn(
+               "flex flex-col h-dvh bg-[url('/background/background-gallery.png')] bg-fixed bg-no-repeat bg-cover bg-[-40vw_center] md:bg-[center_center]  xl:bg-cover",
+               {
+                  "overflow-hidden":
+                     router.state.location.pathname === "/collection",
+               }
+            )}
+         >
+            <Header />
+            <Outlet />
+         </div>
+      );
+   },
 });
